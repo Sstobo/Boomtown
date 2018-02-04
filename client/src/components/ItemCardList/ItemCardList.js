20c4bb846b0dd1f72e4767fee85c80d25dcb51c8
Sean <sean@iMac.hitronhub.home>
2018-02-04 10:18:51 -0800

    tracking down bug with items

--- client/src/components/ItemCardList/ItemCardList.js
+++ client/src/components/ItemCardList/ItemCardList.js
@@ -3,13 +3,17 @@ import Masonry from "react-masonry-component";
 import Styles from "./styles.js";
 import ItemCard from "../ItemCard/ItemCard";
 
-const ItemCardList = ({ items }) => (
+const ItemCardList = ({ items, owner }) => (
   <div className={"list-container"} style={Styles.ListContainer}>
     <Masonry>
     
-      {items.map(item => (
+      {items && items.map(item => (
         <div key={item.id} className={"single-item"} style={Styles.SingleItem}>
-          <ItemCard item={item} key={item.id} />
+          <ItemCard           
+            item={item}
+            owner={item.itemowner.id}
+            key={item.id}
+          />
         </div>
       ))}
     </Masonry>
