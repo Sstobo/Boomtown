a2b022f9a35ba624ebfe804db116a24cb144bc34
Sean <sean@iMac.hitronhub.home>
2018-02-04 09:12:38 -0800

    trying new schema inputs

--- server/api/schema.js
+++ server/api/schema.js
@@ -1,58 +1,60 @@
 module.exports = `
-	type User {
-		id: ID
-		fullname: String
-		email: String
-		bio: String
-		shareditems: [Item]
-		borroweditems: [Item]
-	}
-  type Item {
-    id: ID
-    created: String
-    imageurl: String
-    title: String
-    itemowner: User
-    borrower: User
-    itemurl: String
-    description: String
-    available: Boolean
-    tags: [Tag]
-  }   
-	type Tag {
-		id: ID
-		title: String
-  }
-    
-
-	input AddItemInput {
-		imageurl: String
-		itemowner: ID
-		title: String
-		description: String
-		tags: [TagInput]
-	}
-	input TagInput {
-		id: ID
-		title: String
-	}
-	input UpdateItemInput {
-		id: ID
-		borrower: [BorrowerInput]
-	}
-	input BorrowerInput {
-		id: ID
-  }
+type Tag {
+  title: String
+  id: ID
+}
+type User {
+  email: String
+  fullname: String
+  id: ID
+  bio: String
+  imageurl: String
+  shareditems: [Item]
+}
   
-
-	type Mutation {
-		updateItem(updatedItem: UpdateItemInput): Item
-		createNewItem(newItem: AddItemInput): Item
-	}
-	type Query {
-		items: [Item]
-		users: [User]
-		user(id: ID): User
-		item(id: ID): Item
-	}
+type Item {
+  title: String
+  id: ID
+  itemowner: User
+  borrower: User
+  imageurl: String
+  description: String
+  available: Boolean
+  created: String
+  tags: [Tag]  
+}
+input TagInput {
+  id: ID
+  title: String
+}
+input AddItemInput{
+  imageurl: String
+  title: String
+  description: String
+  itemowner: ID
+  tags: [TagInput]
+}
+input EnterItemInput{
+  title: String
+  itemowner: String
+  description: String
+}
+input UpdateBorrowerInput {
+  available: Boolean
+  borrower: String
+}
+type Mutation {
+  createNewItem(newItem: AddItemInput): Item
+  updateItem(enterItem: EnterItemInput): Item
+  updateBorrower(newBorrower: UpdateBorrowerInput): Item
+   
+}
+type Query {
+  items: [Item]
+  users: [User]
+  user(id: ID): User 
+  item(id: ID): Item
+  
+  
+}
 `;
