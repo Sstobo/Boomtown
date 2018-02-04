2e2a5fbfea5161894c16e25e4bdf73ee979853b1
Sean <sean@iMac.hitronhub.home>
2018-02-04 09:56:04 -0800

    tracking down bug with items

--- client/src/redux/modules/items.js
+++ client/src/redux/modules/items.js
@@ -1,50 +1,117 @@
+const GET_ITEMS_LOADING = "GET_ITEMS_LOADING";
+const GET_ITEMS = "GET_ITEMS";
 const GET_ITEMS_ERROR = "GET_ITEMS_ERROR";
+const ITEMS_URL = "http://localhost:4000/items";
+const USERS_URL = "http://localhost:4000/users";
 const FILTER_ITEMS = "FILTER_ITEMS";
+// port config
+export const getItemsLoading = () => ({
+  type: GET_ITEMS_LOADING
+});
 
-const getItemsError = error => ({
+export const getItems = users => ({
+  type: GET_ITEMS,
+  payload: users
+});
+
+export const getItemsError = error => ({
   type: GET_ITEMS_ERROR,
   payload: error
 });
 
-export const filterItems = values => ({
+// CODE BLOCK FROM COLIN
+export const filterItems = (tags, items) => ({
   type: FILTER_ITEMS,
-  payload: values
+  payload: { tags, items }
 });
 
-//REDUCER
+export const filteredItems = (tags, items) => {
+  if (tags.length === 0 || tags === []) {
+    return items;
+  } else {
+    let result = [];
+    items.forEach(item => {
+    
+      tags.forEach(tag => {
+        if (item.tags.map(tag => tag.title).indexOf(tag) > -1) {
+          result.push(item);
+
+        }
+      });
+    });
+    return result;
+  }
+};
+
+export const fetchItemsAndUsers = () => dispatch => {
+  dispatch(getItemsLoading());
+
+  return Promise.all(
+    [ITEMS_URL, USERS_URL].map(url =>
+      fetch(url).then(response => response.json())
+    )
+  )
+    .then(response => {
+      const [itemsList, usersList] = response;
+
+      const combined = itemsList.map(item => {
+        item.itemowner = usersList.find(user => user.id === item.itemowner);
+       
+        if (item.borrower) {
+          item.borrower = usersList.find(user => user.id === item.borrower);
+        }
+       
+        return item;
+      });
+
+      dispatch(getItems(combined));
+    })
+    .catch(error => dispatch(getItemsError(error)));
+};
+
 export default (
   state = {
-    // isLoading: false,
+    isLoading: false,
     items: [],
-    itemsFilter: [
-      { title: "Electronics", id: 1 },
-      { title: "Household Items", id: 2 },
-      { title: "Musical Instruments", id: 3 },
-      { title: "Physical Media", id: 4 },
-      { title: "Recreational Equipment", id: 5 },
-      { title: "Sporting Goods", id: 6 },
-      { title: "Tools", id: 7 }
-    ],
-    selectedTags: [],
+    itemsFilter: [],
+    tags: [],
     error: ""
   },
   action
 ) => {
   switch (action.type) {
+    case GET_ITEMS_LOADING: {
+      return {
+        ...state,
+        isLoading: true,
+        error: ""
+      };
+    }
+    case GET_ITEMS: {
+      return {
+        ...state,
+        isLoading: false,
+        itemsFilter: action.payload,
+        items: action.payload,
+        error: ""
+      };
+    }
     case GET_ITEMS_ERROR: {
       return {
         ...state,
-
+        isLoading: false,
         error: action.payload
       };
     }
 
     case FILTER_ITEMS: {
-      //call filter
-
+      const filtered = filteredItems(action.payload.tags, action.payload.items);
       return {
         ...state,
-        selectedTags: action.payload
+        isLoading: false,
+        tags: action.payload.tags,
+        items: action.payload.items,
+        itemsFilter: filtered
       };
     }
     default:
