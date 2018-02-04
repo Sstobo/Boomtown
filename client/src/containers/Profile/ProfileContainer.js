400cc14a7a8282ba5f0be237da00e030aecca95f
Sean <sean@iMac.hitronhub.home>
2018-02-04 10:42:49 -0800

    tracking down bug with items

--- client/src/containers/Profile/ProfileContainer.js
+++ client/src/containers/Profile/ProfileContainer.js
@@ -1,73 +1,61 @@
 import React, { Component } from "react";
 import Profile from "./Profile";
-import PropTypes from "prop-types";
-import gql from 'graphql-tag'
-import {graphql} from 'react-apollo'
 import { connect } from "react-redux";
-// import { fetchItemsAndUsers } from "../../redux/modules/profile";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+const fetchUsers = gql`
+  query fetchUser($id: ID) {
+    user(id: $id) {
+      id
+      email
+      fullname
+      bio
+      imageurl
+      shareditems {
+        id
+        title
+        itemowner {
+          id
+          email
+          fullname
+        }
+        borrower {
+          id
+          fullname
+        }
+        created
+        imageurl
+        description
+        available
+        tags {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
 
 class ProfileContainer extends Component {
-  // componentDidMount() {
-  //   this.props.dispatch(fetchItemsAndUsers(this.props.match.params.userid));
-  // }
   render() {
-const {loading, user } = this.props.data;
-console.log(user)
-    return loading ?
-    <p> Loading </p>
-      :
-
-    <Profile items={user.shareditems}  />;
-    
+    const { loading, user } = this.props.data;
+    console.log(this.props.data);
+    return loading ? (
+      <p> Loading</p>
+    ) : (
+      <Profile list={user.shareditems} user={user} />
+    );
   }
 }
-const fetchUser = gql`
-    query fetchUser($id: ID) {
-        user(id: $id) {
-            id
-            email
-            fullname
-            bio
-            shareditems {
-                id
-                title
-                itemowner {
-                    id
-                    email
-                    fullname
-                }
-                borrower {
-                    id
-                    fullname
-                }
-                created
-                imageurl
-                description
-                available
-                tags {
-                    id
-                    title
-                }
-            }
-           
-        }
-    }
- `;
-ProfileContainer.propTypes = {
-  items: PropTypes.array.isRequired
-};
 
-
-
-
-export default graphql(fetchUser, {
-  options: ownProps => ({ 
+export default graphql(fetchUsers, {
+  options: ownProps => ({
     variables: {
-      id: ownProps.match.params.userid
+      id: ownProps.match.params.id // e.g. from React Router!
     }
   })
-})
-(ProfileContainer);
+})(ProfileContainer);
 
 // const mapStateToProps = state => ({
 //   isLoading: state.profile.isLoading,
