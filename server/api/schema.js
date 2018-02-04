da18f8279c477f053395181dae7652e56fba59af
Sean <sean@iMac.hitronhub.home>
2018-02-04 09:09:06 -0800

    complete schema

--- server/api/schema.js
+++ server/api/schema.js
@@ -7,65 +7,52 @@ module.exports = `
 		shareditems: [Item]
 		borroweditems: [Item]
 	}
-	type Item {
+  type Item {
+    id: ID
+    created: String
+    imageurl: String
+    title: String
+    itemowner: User
+    borrower: User
+    itemurl: String
+    description: String
+    available: Boolean
+    tags: [Tag]
+  }   
+	type Tag {
 		id: ID
 		title: String
-		description: String
-		itemowner: User
-		borrower: User
+  }
+  
+
+	input AddItemInput {
 		imageurl: String
-		available: Boolean
-		tags: [Tag]
+		itemowner: ID
+		title: String
+		description: String
+		tags: [TagInput]
 	}
-
-    type Item {
-      id: ID
-      created: String
-      imageurl: String
-      title: String
-      itemowner: User
-      borrower: User
-      itemurl: String
-      description: String
-      available: Boolean
-      tags: [Tag]
-    }  
-    
-    input TagInput {
-      id: ID
-    
-    }
-    input updateItemInput {
-      title: String
-      description: String
-    }
-    input AddItemInput {
-      imageurl: String
-      title: String
-      itemowner: ID
-      description: String
-      tags: [TagInput]
-    }
-    input updateBorrower {
-      available: Boolean  
-      borrower: String
-    }
-
-
-    type AddItemReturn {
-      title: String
-    }
-
-    type Mutation {
-      addItem (newItem : AddItemInput): Item
-      updateItem (currentItem: updateItemInput): Item
-      borrowItem (borrowedItem: updateBorrower): Item
-    }
-    type Query {
-      item(id:ID): Item
-      items: [Item]
-      user(id: ID): User
-      users: [User]
-    }
+	input TagInput {
+		id: ID
+		title: String
+	}
+	input UpdateItemInput {
+		id: ID
+		borrower: [BorrowerInput]
+	}
+	input BorrowerInput {
+		id: ID
+  }
+  
+  
+	type Mutation {
+		updateItem(updatedItem: UpdateItemInput): Item
+		createNewItem(newItem: AddItemInput): Item
+	}
+	type Query {
+		items: [Item]
+		users: [User]
+		user(id: ID): User
+		item(id: ID): Item
+	}
 `;
-
