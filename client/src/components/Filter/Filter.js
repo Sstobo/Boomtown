9519f36d122eeba8dd37dbcbc9d3264016fd7f20
Sean <sean@iMac.hitronhub.home>
2018-02-04 11:57:10 -0800

    changed filter name

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getFilters } from "../../redux/modules/filter";
+import { Filters } from "../../redux/modules/filter";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 
--- client/src/redux/modules/filter.js
+++ client/src/redux/modules/filter.js
@@ -1,68 +1,19 @@
-const GET_PROFILE_LOADING = "GET_PROFILE_LOADING";
-const GET_PROFILE = "GET_PROFILE";
-const GET_PROFILE_ERROR = "GET_PROFILE_ERROR";
-const ITEMS_URL = "http://localhost:4000/items";
-const USERS_URL = "http://localhost:4000/users";
-// port config
-const getProfileLoading = () => ({
-  type: GET_PROFILE_LOADING
-});
 
-const getProfile = users => ({
-  type: GET_PROFILE,
-  payload: users
-});
+const GET_FILTERS = "GET_FILTERS";
 
-const getProfileError = error => ({
-  type: GET_PROFILE_ERROR,
-  payload: error
-});
-
-console.log("filter.js running")
-export const fetchItemsAndUsers = userid => dispatch => {
-  dispatch(getProfileLoading());
-
-  return Promise.all(
-    [`http://localhost:4000/items/?itemowner=${userid}`, USERS_URL].map(url =>
-      fetch(url).then(response => response.json())
-    )
-  )
-    .then(response => {
-      const [itemsList, usersList] = response;
-
-      const combined = itemsList.map(item => {
-        item.itemowner = usersList.find(user => user.id === item.itemowner);
-
-        // const tagged = combined.filter(item => item.tag.title === "Household Items");
-        if (item.borrower) {
-          item.borrower = usersList.find(user => user.id === item.borrower);
-        }
-        return item;
-      });
-
-      dispatch(getProfile(combined));
-    })
-    .catch(error => dispatch(getProfileError(error)));
-};
+export const Filters = filters => ({ type: GET_FILTERS, payload: filters });
 
 export default (
   state = {
-    isLoading: false,
-    items: [],
-    error: ""
+    filter: []
   },
   action
 ) => {
   switch (action.type) {
-    case GET_PROFILE_LOADING: {
-      return { ...state, isLoading: true, error: "" };
+    case GET_FILTERS: {
+      return { ...state, filter: action.payload };
     }
-    case GET_PROFILE: {
-      return { ...state, isLoading: false, items: action.payload, error: "" };
-    }
-    case GET_PROFILE_ERROR: {
-      return { ...state, isLoading: false, error: action.payload };
-    }
+
     default:
       return state;
   }
