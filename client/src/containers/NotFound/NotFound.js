ed2cfac63bd923a5a5393a3e71900916be23a0f2
Sean <sean@iMac.hitronhub.home>
2018-02-04 11:02:36 -0800

    refactoring notfound container

--- client/src/containers/NotFound/NotFound.js
+++ client/src/containers/NotFound/NotFound.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NotFound from './NotFound';
+import NotFoundContainer from './NotFoundContainer';
 
 export default class NotFound extends Component {
   render () {
