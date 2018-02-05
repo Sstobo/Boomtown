8393d42291ca4db09e250cff61d3ad41a9f19d4a
Sean <sean@iMac.hitronhub.home>
2018-02-04 18:22:25 -0800

    index.js delint

--- client/src/containers/Share/ShareContainer.js
+++ client/src/containers/Share/ShareContainer.js
@@ -1,45 +1,11 @@
 import React, { Component } from "react";
-import { graphql, compose } from "react-apollo";
-import gql from "graphql-tag";
+
 import Share from "./Share";
 
 class ShareContainer extends Component {
-  static propTypes = {};
-  constructor() {
-    super();
-    this.state = {
-      title: "",
-      description: "",
-      imageurl: ""
-    };
-  }
-
-
-  render() {
-    return <Share />;
-  }
+	render() {
+		return <Share share={this.share} />;
+	}
 }
 
-const addAnItem = gql`
-  mutation addNewItem(
-    $title: String
-    $description: String
-    $imageurl: String
-    $itemowner: ID
-    $tags: [TagInput]
-  ) {
-    createNewItem(
-      newItem: {
-        title: $title
-        description: $description
-        imageurl: $imageurl
-        itemowner: $itemowner
-        tags: $tags
-      }
-    ) {
-      title
-    }
-  }
-`;
-
-export default graphql(addAnItem)(ShareContainer);
+export default ShareContainer;
